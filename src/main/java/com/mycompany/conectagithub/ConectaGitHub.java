4b1172525d75912278f570c4a4f4cd81ed1ae79d
dperalta <danicdv7@gmail.com>
2025-02-19 18:13:59 +0100

    change 2 main file

--- src/main/java/com/mycompany/conectagithub/ConectaGitHub.java
+++ src/main/java/com/mycompany/conectagithub/ConectaGitHub.java
@@ -4,6 +4,6 @@ package com.mycompany.conectagithub;
 public class ConectaGitHub {
 
     public static void main(String[] args) {
-        System.out.println("Hola Mundo!");
+        System.out.println("Hola!");
     }
 }
